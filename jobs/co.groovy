2c842e8ac692cf394e89f928ba80a7e7d7e62be8
Sumeet Raheja <sumeetraheja@ip-10-15-57-79.eu-west-1.compute.internal>
2018-05-31 15:51:29 +0100

    Upgraded Groovy version

---
+++ jobs/co.groovy
@@ -0,0 +1,9 @@
+import uk.co.argos.middleware.*
+
+String pipeBasePath = 'middleware-jobs-commons'
+
+String viewName = 'COMMONS-VIEW'
+
+println("Hellio")
+
+println(CiCommonBuilder.class.toString())
